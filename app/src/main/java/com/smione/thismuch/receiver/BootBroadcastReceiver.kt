ff2098329112f713eb767db36375d099e246d00a
smione <si.mione@outlook.com>
2025-02-03 18:15:19 +0100

    8 add receiver when boot

---
+++ app/src/main/java/com/smione/thismuch/receiver/BootBroadcastReceiver.kt
@@ -0,0 +1,17 @@
+package com.smione.thismuch.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.smione.thismuch.service.TimeNotificationService
+
+class BootBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.v("BootBroadcastReceiver", "onReceive")
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            val serviceIntent = Intent(context, TimeNotificationService::class.java)
+            context?.startService(serviceIntent)
+        }
+    }
+}
