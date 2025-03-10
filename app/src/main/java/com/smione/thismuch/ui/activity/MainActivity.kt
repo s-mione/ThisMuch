897a38eb184d5a639bf60e348b7996122a94668b
smione <si.mione@outlook.com>
2025-03-10 18:55:07 +0100

    15 get timeNotificationService if it is running

--- app/src/main/java/com/smione/thismuch/ui/activity/MainActivity.kt
+++ app/src/main/java/com/smione/thismuch/ui/activity/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.smione.thismuch.ui.activity
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.util.AttributeSet
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.smione.thismuch.R
 import com.smione.thismuch.model.repository.AccessLogRepositoryInterface
@@ -11,7 +18,30 @@ import com.smione.thismuch.ui.activitycontract.MainActivityContract
 import com.smione.thismuch.ui.fragment.AccessLogListFragment
 import com.smione.thismuch.ui.fragment.MainFragment
 
+interface ServiceConnectionCallback {
+    fun onServiceConnected()
+}
+
 class MainActivity : AppCompatActivity(), MainActivityContract {
+
+    private lateinit var timeNotificationService: TimeNotificationService
+    private var isBound = false
+
+    private var serviceConnectionCallback: ServiceConnectionCallback? = null
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            Log.v("MainActivity", "onServiceConnected")
+            val binder = service as TimeNotificationService.LocalBinder
+            timeNotificationService = binder.getService()
+            isBound = true
+            serviceConnectionCallback?.onServiceConnected()
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            isBound = false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.v("MainActivity", "onCreate")
