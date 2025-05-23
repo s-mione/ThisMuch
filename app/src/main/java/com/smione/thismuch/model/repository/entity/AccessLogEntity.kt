e4e78c3c4dbdfc1776d22f132b15da6634e43ccd
smione <si.mione@outlook.com>
2025-05-23 15:40:04 +0200

    maintenance/6 - make AccessLogListElement and AccessLogEntity have not nullable attributes

--- app/src/main/java/com/smione/thismuch/model/repository/entity/AccessLogEntity.kt
+++ app/src/main/java/com/smione/thismuch/model/repository/entity/AccessLogEntity.kt
@@ -6,8 +6,8 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class AccessLogEntity(
-    @ColumnInfo(name = "time_on") var timeOn: String?,
-    @ColumnInfo(name = "time_off") var timeOff: String?,
-    @ColumnInfo(name = "total_time") var totalTime: String?,
+    @ColumnInfo(name = "time_on") var timeOn: String,
+    @ColumnInfo(name = "time_off") var timeOff: String,
+    @ColumnInfo(name = "total_time") var totalTime: String,
     @PrimaryKey(autoGenerate = true) val id: Int? = null
 )
