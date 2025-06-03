359f606f3e894217d1b083bff78d48862a03f853
smione <si.mione@outlook.com>
2025-06-03 16:03:39 +0200

    Revert "1 - remove recycler view"
    This reverts commit 3659e2e8d574174b2e4f38413135c2fe92f9f873.

---
+++ app/src/main/java/com/smione/thismuch/ui/fragment/recyclerview/AccessLogListRecyclerViewAdapter.kt
@@ -0,0 +1,93 @@
+package com.smione.thismuch.ui.fragment.recyclerview
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.smione.thismuch.databinding.FragmentAccessItemBinding
+import com.smione.thismuch.model.converter.InstantStringConverter
+
+class AccessLogListRecyclerViewAdapter(
+    private val headers: List<String>,
+    private var values: List<AccessLogListElement>)
+    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        private const val VIEW_TYPE_HEADER = 0
+        private const val VIEW_TYPE_ITEM = 1
+    }
+
+    fun updateValues(values: List<AccessLogListElement>, recyclerView: RecyclerView) {
+        Log.v("AccessLogListRecyclerViewAdapter", "updateValues: $values")
+        recyclerView.post {
+            val numberOfItemsAdded = values.size - this.values.size
+            this.values = values
+            notifyItemRangeInserted(0, numberOfItemsAdded)
+        }
+    }
+
+    fun deleteAll(recyclerView: RecyclerView) {
+        Log.v("AccessLogListRecyclerViewAdapter", "delete all")
+        recyclerView.post {
+            notifyItemRangeRemoved(0, values.size)
+            this.values = emptyList()
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) VIEW_TYPE_HEADER else VIEW_TYPE_ITEM
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        return if (viewType == VIEW_TYPE_HEADER) {
+            val binding = FragmentAccessItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+            HeaderViewHolder(binding)
+        } else {
+            val binding = FragmentAccessItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+            ItemViewHolder(binding)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is HeaderViewHolder) {
+            holder.bind(headers)
+        } else if (holder is ItemViewHolder) {
+            val item = values[position - 1]
+            holder.bind(position, item)
+        }
+    }
+
+    override fun getItemCount(): Int = values.size + 1
+
+    inner class HeaderViewHolder(private val binding: FragmentAccessItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(headers: List<String>) {
+            binding.tvItemNumber.text = headers[0]
+            binding.tvTimeOn.text = headers[1]
+            binding.tvTimeOff.text = headers[2]
+            binding.tvTotalTime.text = headers[3]
+        }
+    }
+
+    inner class ItemViewHolder(private val binding: FragmentAccessItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int, item: AccessLogListElement) {
+            binding.tvItemNumber.text = (values.size - position + 1).toString()
+            binding.tvTimeOn.text =
+                item.timeOn?.let { InstantStringConverter.fromInstantToString(it) }
+            binding.tvTimeOff.text =
+                item.timeOff?.let { InstantStringConverter.fromInstantToString(it) }
+            binding.tvTotalTime.text =
+                item.totalTime?.let { InstantStringConverter.fromInstantToString(it) }
+        }
+    }
+}
